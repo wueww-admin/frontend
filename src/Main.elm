30e7863adcc290b4c53c96b7ad1fb33c8181f318
Stefan Siegl <stesie@brokenpipe.de>
2019-06-28 13:39:00 +0200

    add elm sample program (from guide)

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,38 @@
 module Main exposing (main)
 
-import Html exposing (..)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
 
-main : Html msg
+type alias Model =
+    Int
+
+
+main : Platform.Program () Model Msg
 main =
-    text "testling"
+    Browser.sandbox { init = 0, update = update, view = view }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
